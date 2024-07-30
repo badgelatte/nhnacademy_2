a957149f0b1294ce5d48a00d12cf4bac09637185
badgelatte <saucy5939@gmail.com>
2024-07-30 13:34:12 +0900

    Lambda 메소드 참조

--- JavaClassicClass/Lamda/BinaryOp.java
+++ JavaClassicClass/Lamda/BinaryOp.java
@@ -3,6 +3,25 @@ public interface BinaryOp {
     int apply(int i, int j);
 }
 
+class Math{
+    public static int addTwoInt(int i, int j) {
+        return i + j;
+    }
+}
+
+class Algorithm {
+    public static int calc(BinaryOp binder, int i, int j) {
+        return binder.apply(i, j);
+    }
+}
+
+class Test3 {
+    public static void main(String[] args) {
+        // Algorithm.calc((x,y)-> x+y, 1, 2);
+        Algorithm.calc(Math::addTwoInt, 1, 2);
+    }
+}
+
 class Test{
     public static int  calc(BinaryOp binder, int i, int j) {
         return binder.apply(i, j);
