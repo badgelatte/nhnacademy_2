08e6d566416d3c27d0e7da9ece17ecf0f647a3de
badgelatte <saucy5939@gmail.com>
2024-07-30 14:08:29 +0900

    Lambda에서 DoSomething으로 파일 이름 바꿈

---
+++ JavaClassicClass/Lamda/DoSomething.java
@@ -0,0 +1,30 @@
+interface DoSomething{
+    public int doIt(int i);
+}
+
+class realClass implements DoSomething{
+    public int doIt(int end) {
+        int result = 0;
+        for(int i = 0; i <=end; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
+
+class Lamdba {
+    // realClass 구현한 것과 동일
+    // DoSomething d1 = () -> 1;
+    // DoSomething d2 = () ->  {int x = 1; return x;};
+
+    // DoSomething d3 = (int i) -> {return i + 1;};
+
+    // 여기에 int와 () 생략 가능
+    DoSomething d4 = (int end) -> {
+        int result = 0;
+        for(int i = 0; i <=end; i++) {
+            result += i;
+        }
+        return result;
+    };
+}
