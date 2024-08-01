923602797f21422db3425c6a49d9384f0cdf5296
badgelatte <saucy5939@gmail.com>
2024-08-01 16:28:32 +0900

    객체 지향 프로그래밍
    OCP - 개방 폐쇠 워칙에 맞춰서 만듦

--- JavaClassicClass/objectOrientedProgramming/Lab15-2/Test.java
+++ JavaClassicClass/objectOrientedProgramming/Lab15-2/Test.java
@@ -4,7 +4,8 @@ class Test {
     public static void main(String[] args) {
         BankAccount account = new BankAccount("Jason", new BigDecimal(100));
         account.printAccount();
-        BankAccount account2 = new BankAccount("James", new BigDecimal(1000));
+        BankAccount account2 = new LimitedBankAccount("James", new BigDecimal(1000), new BigDecimal(100));
         account2.printAccount();
+        account2.deposit(new BigDecimal("1000"));
     }
 }
