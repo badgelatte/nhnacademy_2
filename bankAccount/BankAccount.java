b6947fbc29e2abf36cfebceef333c27e5e9561f5
badgelatte <saucy5939@gmail.com>
2024-08-01 15:52:25 +0900

    객체 지향 프로그래밍 Lab15-1 실습

---
+++ bankAccount/BankAccount.java
@@ -0,0 +1,50 @@
+import java.math.*;
+
+public class BankAccount {
+    private String accountNumber;
+    private String ownerName;
+    private BigDecimal balance;
+
+    public BankAccount() {
+        this.accountNumber = CreateAccount.createAccountNumber();
+        this.ownerName = "Unknown";
+        this.balance = new BigDecimal("0");
+    }
+
+    public BankAccount(String ownerName, BigDecimal balance) {
+        this.accountNumber = CreateAccount.createAccountNumber();
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+
+    public String getAccountNo() {
+        return this.accountNumber;
+    }
+        
+    public String getOwnerName() {
+        return this.ownerName;
+    }
+        
+    public BigDecimal getBalance() {
+        return this.balance;
+    } 
+
+    public BigDecimal deposit(BigDecimal amount) {
+        this.balance = this.balance.add(amount);
+        return this.balance;
+    }
+
+    public boolean withDraw(BigDecimal amount) {
+        if (amount.compareTo(this.balance) == 1 || amount.compareTo(this.balance) == 0) {
+            return false;
+        }
+        else {
+            balance = balance.subtract(amount);
+            return true;
+        }
+    }
+
+    public void printAccount() {
+        PrintAccount.printAccount(this);
+    }
+}
---
+++ bankAccount/PrintAccount.java
@@ -0,0 +1,7 @@
+public class PrintAccount {
+    public static void printAccount(BankAccount account) {
+        System.out.println("Account Number: " + account.getAccountNo());
+        System.out.println("Owner Name: " + account.getOwnerName());
+        System.out.println("Balance: " + account.getBalance());
+    }
+}
