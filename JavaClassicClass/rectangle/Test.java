ac9ffa2c00211cd2cd0fa21f2315d7ac210c4ff2
badgelatte <saucy5939@gmail.com>
2024-08-02 11:18:27 +0900

    Rectangle과 squre의 차이점

--- JavaClassicClass/rectangle/Test.java
+++ JavaClassicClass/rectangle/Test.java
@@ -1,10 +1,10 @@
 public class Test {
     public static void main(String[] args) {
-        // Rectangle rectangle = new Rectangle();
-        // rectangle.setWidth(10);
-        // rectangle.setHeight(5);
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(10);
+        rectangle.setHeight(5);
 
-        // System.out.println(rectangle.getArea());
+        System.out.println(rectangle.getArea());
 
         Rectangle square = new Square();
         square.setWidth(10);
