00d9af46126b6ed17b02c96707cc98f63d3854b2
badgelatte <saucy5939@gmail.com>
2024-08-05 14:04:13 +0900

    짝수 출력하는 3가지 방식

--- JavaClassicClass/testarray/src/main/java/com/nhnacademy/example3.java
+++ JavaClassicClass/testarray/src/main/java/com/nhnacademy/example3.java
@@ -21,6 +21,23 @@ public class example3 {
         System.out.println();
         System.out.println();
 
+        // 짝수만 출력
+        int[] a2 = new int[] {1,2,3,4,5};
+        for (int i = 0; i < a2.length; i++) {
+            if(a2[i] % 2 == 0) {
+                System.out.print(a2[i] + " ");
+            }
+        }
+        System.out.println();
+
+        for (int value : a2) {
+            if(value % 2 == 0) {
+                System.out.print(value + " ");
+            }
+        }
+        System.out.println();
+
+        Arrays.stream(a2).filter(x -> x % 2 == 0).forEach(x -> System.out.print(x + " "));
     }
     
 }
