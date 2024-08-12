d0c4a526333e2241bc1cae0cfc4777cfb93f2aa8
badgelatte <saucy5939@gmail.com>
2024-08-12 13:30:10 +0900

    예제 2 ArrayStack size(), isEmpty() 추가

--- JavaClassicClass/stack/ArrayStack.java
+++ JavaClassicClass/stack/ArrayStack.java
@@ -1,19 +1,30 @@
 public class ArrayStack implements Stack{
     int[] array;
-    static final int DEFAULT_SIZE = 10;
+    static final int DEFAULT_INCREMENT_SIZE = 10;
     int top = -1;
+    int capacity;
+    int how;
 
     public ArrayStack() {
-        this(DEFAULT_SIZE);
+        this(DEFAULT_INCREMENT_SIZE);
     }
     public ArrayStack(int capacity) {
+        this(capacity, DEFAULT_INCREMENT_SIZE);
+    }
+
+    public ArrayStack(int capacity, int how) {
+        this.capacity = capacity;
+        this.how = how;
         array = new int[capacity];
     }
 
     @Override
     public void push(int elemnent) {
         if((top + 1) >= array.length) {
-            throw new StackFullException();
+            // array = Arrays.copyOf(array, array.length + incrementSize);
+            int[] newArr = new int[array.length + how];
+            System.arraycopy(array, 0, newArr, 0, array.length);
+            array = newArr;
         }
         array[++top] = elemnent;
     }
@@ -30,29 +41,36 @@ public class ArrayStack implements Stack{
     public int peek() {
         return array[top];
     }
-    public static void main(String[] args) {
 
-        ArrayStack stack = new ArrayStack(100);
+    public int size() {
+        return top + 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+        ArrayStack stack = new ArrayStack(3);
     
         stack.push(1);
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.push(2);
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.push(3);
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.push(4);
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.push(5);
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.pop();
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.pop();
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.pop();
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.pop();
-        System.out.println("top : " + stack.peek());
+        System.out.println("top : " + stack.peek() + ", size : " + stack.size());
         stack.pop();
     }
-    
 }
