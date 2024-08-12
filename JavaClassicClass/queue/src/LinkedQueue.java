3bce967b7f1f8b3b9cd6b3a752c31aa885c681ed
badgelatte <saucy5939@gmail.com>
2024-08-12 16:52:46 +0900

    이전에 구현한 건 CircularArrayQueue -> LinkedQueue로 구현 완료

--- JavaClassicClass/queue/src/LinkedQueue.java
+++ JavaClassicClass/queue/src/LinkedQueue.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.NoSuchElementException;
 
 public class LinkedQueue implements Queue{
     class Node{
@@ -16,9 +17,10 @@ public class LinkedQueue implements Queue{
     }
 
     static final int DEFAULT_CAPACITY = 10;
-    int front, rear;
+    Node front, rear;
     int capacity;
     int[] elements;
+    int count;
     
     public LinkedQueue(){
         this(DEFAULT_CAPACITY);
@@ -31,50 +33,81 @@ public class LinkedQueue implements Queue{
 
     @Override
     public void enqueue(int element) {
-        if(rear >= elements.length) {
-            elements = Arrays.copyOf(elements, elements.length + capacity);
+        if(count ==0) {
+            front = rear= new Node(element);
+            count++;
+        } else {
+            rear.next = new Node(element);
+            rear = rear.next;
+            count++;
         }
-        elements[rear++] = element;
     }
 
     @Override
     public int dequeue() {
-        int data = elements[front];
-        elements[front++] = 0;
-        if(front > capacity) {
-            elements = Arrays.copyOfRange(elements, capacity, capacity+rear-front);
-            front -= capacity;
-            rear -= capacity;
+
+        if(count == 0) {
+            throw new NoSuchElementException();
         }
+
+        int data = front.data;
+        front = front.next;
+        --count;
+        if(front == null) {
+            rear = null;
+        }
+
         return data;
     }
 
     @Override
     public int peek() {
-        return elements[front];
+        if(count ==0) {
+            throw new NoSuchElementException();
+        }
+        return front.data;
     }
 
     public int size(){
-        return rear- front;
+        return count;
     }
 
     public boolean isEmpty(){
-        return rear == front;
+        return count == 0;
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
     public String toString(){
-        String result = "[";
-        if(front != rear) {
-            result += elements[front];
+        // String result = "[";
+        // if(front != rear) {
+        //     result += elements[front];
+        // }
+        // for(int i = front+1; i < rear; i++){
+        //     result += (","+elements[i]);
+        // }
+        
+        // return result += "]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node p = front;
+        if(p != null) {
+            sb.append(p.data);
+            p = p.next;
         }
-        for(int i = front+1; i < rear; i++){
-            result += (","+elements[i]);
+        while(p!= null) {
+            sb.append(", ").append(p.data);
+            p = p.next;
         }
         
-        return result += "]";
+        sb.append("]");
+        return sb.toString();
     }
 
-    public static void main2(String[] args) {
+    public static void main(String[] args) {
         Queue queue = new LinkedQueue();
 
         System.out.println("queue : " + queue);
@@ -100,27 +133,7 @@ public class LinkedQueue implements Queue{
         System.out.println("queue : " + queue);
     }
 
-    public static void main(String[] args) {
-        ArrayQueue queue = new ArrayQueue(2);
-        LinkedQueue circularQueue = new LinkedQueue(2);
 
-        System.out.printf("%10s | %10s | %10s | %10s | %10s | %10s%n",
-                "queue", "Capacity", "Elements", "circularQueue", "Capacity", "Elements");
-        for (int i = 0; i < 10; i++) {
-            System.out.printf("%10s | %10d | %10s | %10s | %10d | %10s%n",
-                    "queue", queue.capacity, queue.toString(),
-                    "circularQueue", circularQueue.capacity, circularQueue.toString());
-            queue.enqueue(i);
-            circularQueue.enqueue(i);
-            System.out.printf("%10s | %10d | %10s | %10s | %10d | %10s%n",
-                    "queue", queue.capacity, queue.toString(),
-                    "circularQueue", circularQueue.capacity, circularQueue.toString());
-            queue.dequeue();
-            circularQueue.dequeue();
-        }
-        System.out.printf("%10s | %10d | %10s | %10s | %10d | %10s%n",
-                "queue", queue.capacity, queue.toString(),
-                "circularQueue", circularQueue.capacity, circularQueue.toString());
-    }
+    
 
 }
