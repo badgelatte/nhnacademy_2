0e6a7afedfcb0e4799f8ff186c42f1b1c9cc3dac
badgelatte <saucy5939@gmail.com>
2024-08-14 16:21:24 +0900

    양의 진수의 16진수, 원하는 진수를 표현을 포함하는 문자열을 반환하는 재귀 함수 작성

--- JavaClassicClass/recursion/ToBinary.java
+++ JavaClassicClass/recursion/ToBinary.java
@@ -1,4 +1,5 @@
 public class ToBinary {
+    // 10진수 -> 2진수
     static String toBinary(int n) {
         if(n == 0) {
             return "0";
@@ -26,8 +27,33 @@ public class ToBinary {
         }
         // return result;
     }
+
+    static final char[] digits = {
+        '0','1','2','3',
+        '4','5','6','7',
+        '8','9', 'a','b',
+        'c','d','e','f'};
+
+    // 10진수 -> 16진수
+    static void toConversion(StringBuilder sb, int n) {
+        if(n > 15) {
+            toConversion(sb, n/16);
+        }
+        sb.append(digits[n%16]);
+    }
+
+    // 10진수 -> base진수(내가 원하는 진수로 가능(ex. 2,4,8,16))
+    static void toConversion(StringBuilder sb, int n, int base) {
+        if(n > base-1) {
+            toConversion(sb, n/ base, base);
+        }
+        sb.append(digits[n % base]);
+    }
+
+
     public static void main(String[] args) {
         int[] values = {1,5,13,1023};
+        int base = 16;
 
         for (int i : values) {
             System.out.printf("toBinary(%d) = %s%n", i, toBinary(i));
@@ -37,6 +63,18 @@ public class ToBinary {
         for (int i : values) {
             toBinary(sb, i);
             System.out.printf("toBinary(%d) = %s%n", i, sb.toString());
+        }
+
+        sb = sb.delete(0, sb.length());
+        for (int i : values) {
+            toConversion(sb, i);
+            System.out.printf("toConversion(%d) = %s%n", i, sb.toString());
+        }
+
+        sb = sb.delete(0, sb.length());
+        for (int i : values) {
+            toConversion(sb, i, base);
+            System.out.printf("toConversion(%d) = %s%n", i, sb.toString());
         }
     }
 }
