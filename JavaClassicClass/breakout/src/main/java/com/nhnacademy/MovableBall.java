b6458518d7f279792d8313372c8f490a13b39cfc
badgelatte <saucy5939@gmail.com>
2024-08-19 15:57:20 +0900

    움직이는 ball class

---
+++ JavaClassicClass/breakout/src/main/java/com/nhnacademy/MovableBall.java
@@ -0,0 +1,43 @@
+package com.nhnacademy;
+
+import java.awt.Color;
+
+public class MovableBall extends PaintableBall {
+    int dx;
+    int dy;
+    
+    public MovableBall(int x, int y, int radius) {
+        super(x, y, radius);
+    }
+    
+    public MovableBall(int x, int y, int radius, Color color) {
+        super(x, y, radius, color);
+    }
+
+    public int getDx() {
+        return dx;
+    }
+
+    public int getDy() {
+        return dy;
+    }
+
+    public void setDx(int dx) {
+        this.dx = dx;
+    }
+
+    public void setDy(int dy) {
+        this.dy = dy;
+    }
+
+    public void move(){
+        moveTo(getX() + dx, getY() + dy);
+    }
+
+    public void moveTo(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+    
+}
