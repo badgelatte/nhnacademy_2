af6a15f1a40c05cd6e1e21f1e23ba6ee38a35b95
badgelatte <saucy5939@gmail.com>
2024-08-19 15:57:09 +0900

    ball 테스트

--- JavaClassicClass/breakout/src/main/java/com/nhnacademy/Main.java
+++ JavaClassicClass/breakout/src/main/java/com/nhnacademy/Main.java
@@ -1,5 +1,6 @@
 package com.nhnacademy;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 
@@ -8,6 +9,7 @@ public class Main extends Frame {
     static final int y = 200;
     static final int radius = 50;
     PaintableBall ball = new PaintableBall(x, y, radius);
+    PaintableBall ball2 = new PaintableBall(x+ 100, y + 100, radius, Color.BLUE);
     
     public static void main(String[] args) {
         // Ball ball = new Ball(100, 200, 30);
@@ -31,5 +33,8 @@ public class Main extends Frame {
         super.paint(g);
         System.out.println("Paint called");
         ball.paint(g);
+        ball2.paint(g);
     }
 }
+
+// frame = 전체 panel은 frame 위에 붙인느ㄷ;ㅈㅇ=는      삼
