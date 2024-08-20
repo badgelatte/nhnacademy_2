71ad76e12b0cbd38f5a0a18cb104a66e7d027724
badgelatte <saucy5939@gmail.com>
2024-08-20 15:55:41 +0900

    다른 것과 만날 경우 반대로 움직이는 bounce 메소드 추가

--- JavaClassicClass/breakout/src/main/java/com/nhnacademy/MovableBall.java
+++ JavaClassicClass/breakout/src/main/java/com/nhnacademy/MovableBall.java
@@ -1,11 +1,12 @@
 package com.nhnacademy;
 
 import java.awt.Color;
+import java.awt.Rectangle;
 
 /*
  * 그림이 가로, 세로 얼마나 움직이는가를 설정하는 class
  */
-public class MovableBall extends PaintableBall {
+public class MovableBall extends PaintableBall implements Movable{
     int dx;
     int dy;
     
@@ -43,6 +44,20 @@ public class MovableBall extends PaintableBall {
         setX(x);
         setY(y);
     }
+ 
+    public void bounce(Bounded bounded) {
+        if(getBounds().intersects(bounded.getBounds())) {
+            // 두 영역의 공통 영역 구하기
+            Rectangle intersection = getBounds().intersection(bounded.getBounds());
 
-    
+            // ball과 공통 영역 / 다른 ball과 공통 영역이 각각 길이 혹은 높이 모두 맞지 않는다면 다른 방향으로 바꾸어라 
+            if (intersection.getWidth() != getBounds().getWidth() && intersection.getWidth() != bounded.getWidth()) {
+                setDx(-getDx());
+            }
+
+            if(intersection.getHeight() != getBounds().getHeight() && intersection.getHeight() != bounded.getHeight()) {
+                setDy(-getDy());
+            }
+        }
+    }
 }
