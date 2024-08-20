c57e2c8cdd830892ffbff1c4200557cfc3d94e51
badgelatte <saucy5939@gmail.com>
2024-08-20 15:51:22 +0900

    원을 이루는 테두리를 추가

--- JavaClassicClass/breakout/src/main/java/com/nhnacademy/PaintableBall.java
+++ JavaClassicClass/breakout/src/main/java/com/nhnacademy/PaintableBall.java
@@ -3,7 +3,7 @@ package com.nhnacademy;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class PaintableBall extends Ball {
+public class PaintableBall extends Ball implements Paintable{
     public static final Color DEFAULT_COLOR = Color.black;    
     Color color;
 
@@ -19,5 +19,7 @@ public class PaintableBall extends Ball {
     public void paint(Graphics g) {
         g.setColor(color);
         g.fillOval(x - radius, y - radius, 2 * radius, 2 * radius);
+        g.setColor(Color.BLACK);
+        g.drawRect(x - radius, y - radius, 2 * radius, 2 * radius);
     }
 }
